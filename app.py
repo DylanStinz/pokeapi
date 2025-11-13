4591a84f3c121f01e28b8b10b917defb8cc60482
DylanStinz <23308060610028@cetis61.edu.mx>
2025-11-12 19:19:23 -0600

    ns

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask , render_template , request , redirect , url_for, flash
 import requests
 
+
 app= Flask(__name__)
 
 app.config["SECRET_KEY"] = "una_clave_muy_larga_y_dificil_de_adivinar"
+
+API = "https://pokeapi.co/api/v2/"
 
 @app.route("/")
 
@@ -16,6 +19,26 @@ def search():
 
     if not pokemon_name:
         flash("Por favor, ingresa el nombre de un Pokémon.", "error")
+        return redirect(url_for("index"))
+    try:
+        resp = requests.get(f"{API}{pokemon_name}")
+        if resp.status_code == 200:
+            pokemon_data = resp.json()
+            pokemon_info = {
+                "name": pokemon_data["name"].title(),
+                "id": pokemon_data["id"],
+                "height": pokemon_data["height"],
+                "weight": pokemon_data["weight"],
+                "image": pokemon_data["sprites"]["front_default"],
+                "types": [t["type"]["name"].title() for t in pokemon_data["types"]],
+                "abilities": [a["ability"]["name"].title() for a in pokemon_data["abilities"]],
+            }
+            return render_template("pokemon.html", pokemon=pokemon_info)
+        else:
+            flash(f"Pokémon '{pokemon_name}' no encontrado. Intenta con otro nombre.", "error")
+            return redirect(url_for("index"))
+    except requests.exceptions.RequestException as e:
+        flash("Error al buscar el pokemon", "error")
         return redirect(url_for("index"))
 if __name__ == "__main__":
     app.run(debug=True)
